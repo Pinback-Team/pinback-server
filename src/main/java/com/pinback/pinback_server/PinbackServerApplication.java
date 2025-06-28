bf55d2a80ead46f49bb237afcef1b6fd9ad97097
rootTiket <l20078829@gmail.com>
2025-06-29 00:26:18 +0900

    feat: BaseEntity 추가

--- src/main/java/com/pinback/pinback_server/PinbackServerApplication.java
+++ src/main/java/com/pinback/pinback_server/PinbackServerApplication.java
@@ -2,8 +2,10 @@ package com.pinback.pinback_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PinbackServerApplication {
 
 	public static void main(String[] args) {
