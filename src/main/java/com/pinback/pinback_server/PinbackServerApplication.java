b4b1dbfbd363b481316dc5692f6f1d6f18c2cc97
rootTiket <l20078829@gmail.com>
2025-06-28 16:02:40 +0900

    init

---
+++ src/main/java/com/pinback/pinback_server/PinbackServerApplication.java
@@ -0,0 +1,13 @@
+package com.pinback.pinback_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinbackServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinbackServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pinback/pinback_server/PinbackServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinback.pinback_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinbackServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
