9c265ead7cda80781e17e0e44236ac76db6b150a
rootTiket <l20078829@gmail.com>
2025-06-29 00:23:40 +0900

    feat: 스웨거 추가

---
+++ src/main/java/com/pinback/pinback_server/global/config/swagger/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.pinback.pinback_server.global.config.swagger;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class SwaggerConfig {
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+			.info(new Info()
+				.title("Pinback API")
+				.description("Pinback 서비스 API 문서")
+				.version("v1.0.0"))
+			.servers(List.of(
+				new Server().url("http://localhost:8080").description("개발 서버")))
+			.components(new Components()
+				.addSecuritySchemes("bearerAuth",
+					new SecurityScheme()
+						.type(SecurityScheme.Type.HTTP)
+						.scheme("bearer")
+						.bearerFormat("JWT")));
+	}
+}
