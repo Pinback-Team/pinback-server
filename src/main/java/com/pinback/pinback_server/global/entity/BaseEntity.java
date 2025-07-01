82cffe9e97fd602cfe4418649920259cef6032f0
rootTiket <l20078829@gmail.com>
2025-07-02 01:36:45 +0900

    feat: soft delete 구현

--- src/main/java/com/pinback/pinback_server/global/entity/BaseEntity.java
+++ src/main/java/com/pinback/pinback_server/global/entity/BaseEntity.java
@@ -2,6 +2,8 @@ package com.pinback.pinback_server.global.entity;
 
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -14,6 +16,8 @@ import lombok.Getter;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@SQLDelete(sql = "UPDATE category SET deleted = true WHERE id = ?")
+@SQLRestriction("deleted = false")
 public class BaseEntity {
 	@Column(name = "created_at")
 	@CreatedDate
@@ -22,4 +26,7 @@ public class BaseEntity {
 	@Column(name = "updated_at")
 	@LastModifiedDate
 	private LocalDateTime updatedAt;
+
+	@Column(name = "deleted_at")
+	private boolean deletedAt;
 }
