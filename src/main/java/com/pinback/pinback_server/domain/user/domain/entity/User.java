0574933231319959e51f37dbfe2a442b21f79a42
rootTiket <l20078829@gmail.com>
2025-07-02 01:19:33 +0900

    feat: 유저 도메인 생성

---
+++ src/main/java/com/pinback/pinback_server/domain/user/domain/entity/User.java
@@ -0,0 +1,41 @@
+package com.pinback.pinback_server.domain.user.domain.entity;
+
+import com.google.type.DateTime;
+import com.pinback.pinback_server.global.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Builder(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(name = "user_id")
+	private Long id;
+
+	@Column(name = "remind_default", nullable = false)
+	private DateTime remindDefault;
+
+	@Column(name = "acorn_count", nullable = false)
+	private Long acornCount;
+
+	public static User create(DateTime remindDefault) {
+		return User.builder()
+			.remindDefault(remindDefault)
+			.acornCount(0L)
+			.build();
+	}
+}
