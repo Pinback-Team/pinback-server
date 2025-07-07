d214bca4cce4751718855f0b3ec5d88c8ae37261
rootTiket <l20078829@gmail.com>
2025-07-07 18:53:25 +0900

    feat: 아티클 생성 로직 구현

---
+++ src/main/java/com/pinback/pinback_server/domain/article/application/ArticleManagementUsecase.java
@@ -0,0 +1,30 @@
+package com.pinback.pinback_server.domain.article.application;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.pinback.pinback_server.domain.article.application.command.ArticleCreateCommand;
+import com.pinback.pinback_server.domain.article.domain.entity.Article;
+import com.pinback.pinback_server.domain.article.domain.service.ArticleSaveService;
+import com.pinback.pinback_server.domain.category.domain.entity.Category;
+import com.pinback.pinback_server.domain.category.domain.service.CategoryGetService;
+import com.pinback.pinback_server.domain.user.domain.entity.User;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ArticleManagementUsecase {
+
+	private final CategoryGetService categoryGetService;
+	private final ArticleSaveService articleSaveService;
+
+	//TODO: 리마인드 로직 추가 필요
+	//TODO: 자신의 카테고리가 아닐경우 예외처리
+	public void createArticle(User user, ArticleCreateCommand command) {
+		Category category = categoryGetService.getCategory(command.categoryId());
+		Article article = Article.create(command.url(), command.memo(), user, category);
+		articleSaveService.save(article);
+	}
+}
