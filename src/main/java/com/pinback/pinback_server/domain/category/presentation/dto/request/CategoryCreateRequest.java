9b78381495c9b2f3cc5ca95c3b35e0f615e3fb52
ose0221 <ose0221@naver.com>
2025-07-08 06:27:55 +0900

    feat: 카테고리 이름 글자수 제한

--- src/main/java/com/pinback/pinback_server/domain/category/presentation/dto/request/CategoryCreateRequest.java
+++ src/main/java/com/pinback/pinback_server/domain/category/presentation/dto/request/CategoryCreateRequest.java
@@ -3,9 +3,11 @@ package com.pinback.pinback_server.domain.category.presentation.dto.request;
 import com.pinback.pinback_server.domain.category.application.command.CategoryCreateCommand;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record CategoryCreateRequest(
 	@NotBlank(message = "카테고리 이름은 비어있을 수 없습니다.")
+	@Pattern(regexp = "^[\\\\S]*$", message = "카테고리 이름에 공백을 포함할 수 없습니다.")
 	String categoryName
 ) {
 	public CategoryCreateCommand toCommand() {
