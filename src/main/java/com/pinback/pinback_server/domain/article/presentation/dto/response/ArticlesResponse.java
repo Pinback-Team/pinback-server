51a71ac5e3b005552012574264ededccfc45f47a
rootTiket <l20078829@gmail.com>
2025-07-08 02:04:33 +0900

    fix: 읽음 여부 추가

--- src/main/java/com/pinback/pinback_server/domain/article/presentation/dto/response/ArticlesResponse.java
+++ src/main/java/com/pinback/pinback_server/domain/article/presentation/dto/response/ArticlesResponse.java
@@ -5,13 +5,15 @@ import com.pinback.pinback_server.domain.article.domain.entity.Article;
 public record ArticlesResponse(
 	long articleId,
 	String url,
-	String memo
+	String memo,
+	boolean isRead
 ) {
 	public static ArticlesResponse from(Article article) {
 		return new ArticlesResponse(
 			article.getId(),
 			article.getUrl(),
-			article.getMemo()
+			article.getMemo(),
+			article.isRead()
 		);
 	}
 }
