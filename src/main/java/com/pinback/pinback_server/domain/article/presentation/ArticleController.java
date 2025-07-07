0953f1bed139f215b0ce10ed5ff7ea96edf8d898
rootTiket <l20078829@gmail.com>
2025-07-08 01:59:52 +0900

    feat: 컨트롤러 생성

--- src/main/java/com/pinback/pinback_server/domain/article/presentation/ArticleController.java
+++ src/main/java/com/pinback/pinback_server/domain/article/presentation/ArticleController.java
@@ -1,15 +1,19 @@
 package com.pinback.pinback_server.domain.article.presentation;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pinback.pinback_server.domain.article.application.ArticleManagementUsecase;
 import com.pinback.pinback_server.domain.article.presentation.dto.request.ArticleCreateRequest;
 import com.pinback.pinback_server.domain.article.presentation.dto.response.ArticleDetailResponse;
+import com.pinback.pinback_server.domain.article.presentation.dto.response.ArticlesResponse;
 import com.pinback.pinback_server.domain.user.domain.entity.User;
 import com.pinback.pinback_server.global.common.annotation.CurrentUser;
 import com.pinback.pinback_server.global.common.dto.ResponseDto;
@@ -32,6 +36,14 @@ public class ArticleController {
 	@GetMapping("/details/{articleId}")
 	public ResponseDto<ArticleDetailResponse> getArticleDetails(@PathVariable Long articleId) {
 		ArticleDetailResponse response = articleManagementUsecase.getArticleDetail(articleId);
+		return ResponseDto.ok(response);
+	}
+
+	@GetMapping
+	public ResponseDto<List<ArticlesResponse>> getAll(@CurrentUser User user, @RequestParam int pageNumber,
+		@RequestParam int pageSize) {
+
+		List<ArticlesResponse> response = articleManagementUsecase.getAllArticles(user, pageNumber, pageSize);
 		return ResponseDto.ok(response);
 	}
 
