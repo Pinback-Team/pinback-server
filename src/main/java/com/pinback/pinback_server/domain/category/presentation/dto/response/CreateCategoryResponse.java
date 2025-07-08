eadfff8f59c1651910bf949cde42f253fedfb0ed
ose0221 <ose0221@naver.com>
2025-07-08 16:41:37 +0900

    refactor: id 타입 변경

--- src/main/java/com/pinback/pinback_server/domain/category/presentation/dto/response/CreateCategoryResponse.java
+++ src/main/java/com/pinback/pinback_server/domain/category/presentation/dto/response/CreateCategoryResponse.java
@@ -1,10 +1,10 @@
 package com.pinback.pinback_server.domain.category.presentation.dto.response;
 
 public record CreateCategoryResponse(
-	Long id,
+	long id,
 	String categoryName
 ) {
-	public static CreateCategoryResponse of(Long id, String categoryName) {
+	public static CreateCategoryResponse of(long id, String categoryName) {
 		return new CreateCategoryResponse(id, categoryName);
 	}
 }
