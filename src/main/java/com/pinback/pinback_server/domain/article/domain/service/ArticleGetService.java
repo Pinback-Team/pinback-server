2756fd8074667fb8a8f7869c1478ab8431743c8e
rootTiket <l20078829@gmail.com>
2025-07-09 17:30:45 +0900

    refactor: EOF 적용

--- src/main/java/com/pinback/pinback_server/domain/article/domain/service/ArticleGetService.java
+++ src/main/java/com/pinback/pinback_server/domain/article/domain/service/ArticleGetService.java
@@ -23,31 +23,31 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class ArticleGetService {
-	
+
 	private final ArticleRepository articleRepository;
-	
+
 	public boolean checkExistsByUserAndUrl(User user, String url) {
 		return articleRepository.existsByUserAndUrl(user, url);
 	}
-	
+
 	public Article findById(long articleId) {
 		return articleRepository.findById(articleId).orElseThrow(ArticleNotFoundException::new);
 	}
-	
+
 	public ArticlesWithUnreadCount findAll(UUID userId, PageRequest pageRequest) {
 		return articleRepository.findAllCustom(userId, pageRequest);
 	}
-	
+
 	public ArticlesWithUnreadCount findAllByCategory(UUID userId, Category category, PageRequest pageRequest) {
 		return articleRepository.findAllByCategory(userId, category.getId(), pageRequest);
 	}
-	
+
 	// feat/#23 브랜치에서 추가된 메서드
 	public Page<Article> findTodayRemind(UUID userId, LocalDateTime today, Pageable pageable) {
 		LocalDateTime startAt = today.minusDays(1L).plusMinutes(1L);
 		return articleRepository.findTodayRemind(userId, pageable, startAt, today);
 	}
-	
+
 	// dev 브랜치에서 추가된 메서드
 	public Optional<Article> findRecentByUser(User user) {
 		return articleRepository.findRecentArticleByUser(user);
