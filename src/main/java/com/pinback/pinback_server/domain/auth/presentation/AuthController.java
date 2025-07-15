af06a5a815287e3ae3f054c9408f1ea1b28300e1
rootTiket <l20078829@gmail.com>
2025-07-16 02:26:50 +0900

    feat: 컨트롤러 생성

--- src/main/java/com/pinback/pinback_server/domain/auth/presentation/AuthController.java
+++ src/main/java/com/pinback/pinback_server/domain/auth/presentation/AuthController.java
@@ -1,13 +1,16 @@
 package com.pinback.pinback_server.domain.auth.presentation;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pinback.pinback_server.domain.auth.application.AuthUsecase;
 import com.pinback.pinback_server.domain.auth.presentation.dto.request.SignUpRequest;
 import com.pinback.pinback_server.domain.auth.presentation.dto.response.SignUpResponse;
+import com.pinback.pinback_server.domain.auth.presentation.dto.response.TokenResponse;
 import com.pinback.pinback_server.global.common.dto.ResponseDto;
 
 import jakarta.validation.Valid;
@@ -23,5 +26,12 @@ public class AuthController {
 	public ResponseDto<SignUpResponse> signup(@Valid @RequestBody SignUpRequest signUpRequest) {
 		SignUpResponse response = authUsecase.signUp(signUpRequest.toCommand());
 		return ResponseDto.created(response);
+	}
+
+	@GetMapping("/token")
+	public ResponseDto<TokenResponse> getToken(@Valid @RequestParam String email) {
+		TokenResponse response = authUsecase.getToken(email);
+
+		return ResponseDto.ok(response);
 	}
 }
