893b7b668d2f5aec5318135f754eac443b1239f3
ose0221 <ose0221@naver.com>
2025-07-15 21:58:56 +0900

    refactor: 카테고리 수정 응답 DTO 필드명 변경

--- src/main/java/com/pinback/pinback_server/domain/category/presentation/dto/response/UpdateCategoryResponse.java
+++ src/main/java/com/pinback/pinback_server/domain/category/presentation/dto/response/UpdateCategoryResponse.java
@@ -3,7 +3,7 @@ package com.pinback.pinback_server.domain.category.presentation.dto.response;
 import com.pinback.pinback_server.domain.category.domain.entity.Category;
 
 public record UpdateCategoryResponse(
-	long id,
+	long categoryId,
 	String categoryName
 ) {
 	public static UpdateCategoryResponse from(Category category) {
