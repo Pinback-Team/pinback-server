8546361561f432ce8b37f372128b24179784a593
rootTiket <l20078829@gmail.com>
2025-07-16 12:18:06 +0900

    feat: 테스트용 목업 데이터 생성 로직 구현

--- src/main/java/com/pinback/pinback_server/domain/test/application/TestUsecase.java
+++ src/main/java/com/pinback/pinback_server/domain/test/application/TestUsecase.java
@@ -1,8 +1,15 @@
 package com.pinback.pinback_server.domain.test.application;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
+import com.pinback.pinback_server.domain.article.domain.entity.Article;
+import com.pinback.pinback_server.domain.article.domain.service.ArticleSaveService;
+import com.pinback.pinback_server.domain.category.domain.entity.Category;
+import com.pinback.pinback_server.domain.category.domain.service.CategoryGetService;
 import com.pinback.pinback_server.domain.test.presentation.dto.request.PushTestRequest;
+import com.pinback.pinback_server.domain.user.domain.entity.User;
 import com.pinback.pinback_server.infra.firebase.FcmService;
 
 import lombok.RequiredArgsConstructor;
@@ -11,8 +18,21 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TestUsecase {
 	private final FcmService fcmService;
+	private final CategoryGetService categoryGetService;
+	private final ArticleSaveService articleSaveService;
 
 	public void pushTest(PushTestRequest request) {
 		fcmService.sendNotification(request.fcmToken(), request.message());
 	}
+
+	public void createByCategory(User user, long categoryId) {
+		Category category = categoryGetService.findById(categoryId);
+
+		for (int i = 0; i < 5; i++) {
+			articleSaveService.save(Article.create(
+				UUID.randomUUID().toString(), "testMemo", user, category, null
+			));
+		}
+	}
+
 }
