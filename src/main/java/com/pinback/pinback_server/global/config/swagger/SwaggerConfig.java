9333f8a2c55639e126f20c03a6cf14d9cbe4a017
rootTiket <l20078829@gmail.com>
2025-07-18 01:20:57 +0900

    refactor: 배포서버 주소 변경

--- src/main/java/com/pinback/pinback_server/global/config/swagger/SwaggerConfig.java
+++ src/main/java/com/pinback/pinback_server/global/config/swagger/SwaggerConfig.java
@@ -22,7 +22,7 @@ public class SwaggerConfig {
 				.description("Pinback 서비스 API 문서")
 				.version("v1.0.0"))
 			.servers(List.of(
-				new Server().url("https://54.180.92.66.nip.io").description("배포 서버"),
+				new Server().url("https://api.pinback.today").description("배포 서버"),
 				new Server().url("http://localhost:8080").description("로컬 서버")
 			))
 			.addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
