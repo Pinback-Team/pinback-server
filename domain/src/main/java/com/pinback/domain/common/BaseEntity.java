c20072b1bce005606241efa99bd2a6299675e4d3
rootTiket <l20078829@gmail.com>
2025-08-18 18:14:15 +0900

    fix: 기존 비즈니스 규칙에 맞게 baseEntity 수정

--- domain/src/main/java/com/pinback/domain/common/BaseEntity.java
+++ domain/src/main/java/com/pinback/domain/common/BaseEntity.java
@@ -17,10 +17,10 @@ import lombok.Getter;
 public abstract class BaseEntity {
 
 	@CreatedDate
-	@Column(name = "created_at", nullable = false, updatable = false)
+	@Column(name = "created_at", updatable = false)
 	private LocalDateTime createdAt;
 
 	@LastModifiedDate
-	@Column(name = "updated_at", nullable = false)
+	@Column(name = "updated_at")
 	private LocalDateTime updatedAt;
 }
