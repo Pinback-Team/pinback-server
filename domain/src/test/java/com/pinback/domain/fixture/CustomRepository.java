408cc4a1a48c7f6f6a54501ff58064a3cb05e05d
rootTiket <l20078829@gmail.com>
2025-08-18 18:32:33 +0900

    chore: 테스트 레포지토리 설정 추가

---
+++ domain/src/test/java/com/pinback/domain/fixture/CustomRepository.java
@@ -0,0 +1,26 @@
+package com.pinback.domain.fixture;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import jakarta.persistence.EntityManager;
+
+@Repository
+public class CustomRepository {
+	@Autowired
+	private EntityManager entityManager;
+
+	@Transactional
+	public void clearAndReset() {
+		entityManager.createNativeQuery("DELETE FROM article_migration").executeUpdate();
+		entityManager.createNativeQuery("DELETE FROM push_subscription_migration").executeUpdate();
+		entityManager.createNativeQuery("DELETE FROM category_migration").executeUpdate();
+		entityManager.createNativeQuery("DELETE FROM users_migration").executeUpdate();
+
+		entityManager.createNativeQuery("ALTER TABLE article_migration ALTER COLUMN article_id RESTART WITH 1")
+			.executeUpdate();
+		entityManager.createNativeQuery("ALTER TABLE category_migration ALTER COLUMN category_id RESTART WITH 1")
+			.executeUpdate();
+	}
+}
