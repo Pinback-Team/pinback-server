9b44db85139a4f2e5479d39c5e48de62d4745808
rootTiket <l20078829@gmail.com>
2025-08-20 13:48:37 +0900

    feat: 카테고리 application 마이그레이션

---
+++ application/src/main/java/com/pinback/application/category/port/in/GetCategoryPort.java
@@ -0,0 +1,11 @@
+package com.pinback.application.category.port.in;
+
+import com.pinback.application.category.dto.response.CategoriesForDashboardResponse;
+import com.pinback.application.category.dto.response.CategoriesForExtensionResponse;
+import com.pinback.domain.user.entity.User;
+
+public interface GetCategoryPort {
+	CategoriesForExtensionResponse getAllCategoriesForExtension(User user);
+
+	CategoriesForDashboardResponse getAllCategoriesForDashboard(User user);
+}
