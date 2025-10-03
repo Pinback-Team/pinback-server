dcbc035c2285f0f0187936a9879358265ca989d3
rootTiket <l20078829@gmail.com>
2025-10-03 11:54:56 +0900

    HOTFIX: 알림을 서버에서 보내도록 변경

--- infrastructure/src/main/java/com/pinback/infrastructure/firebase/FcmService.java
+++ infrastructure/src/main/java/com/pinback/infrastructure/firebase/FcmService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,6 +16,10 @@ public class FcmService {
 		try {
 			Message.Builder messageBuilder = Message.builder()
 				.setToken(token)
+				.setNotification(Notification.builder()
+					.setTitle("pinback")
+					.setBody("저장한 문구를 확인해보세요")
+					.build())
 				.putData("url", url);
 
 			FirebaseMessaging.getInstance().send(messageBuilder.build());
