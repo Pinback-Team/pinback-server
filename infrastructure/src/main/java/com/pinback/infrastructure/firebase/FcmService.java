862bdcabd2efd84e3724c6da52a431f9f6db9638
ose0221 <ose0221@naver.com>
2025-11-25 18:14:29 +0900

    HOTFIX: 알림 로그 추가

--- infrastructure/src/main/java/com/pinback/infrastructure/firebase/FcmService.java
+++ infrastructure/src/main/java/com/pinback/infrastructure/firebase/FcmService.java
@@ -13,6 +13,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class FcmService {
 	public void sendNotification(String token, String url) {
+		if (token == null || token.isEmpty()) {
+			log.error("FCM 전송 실패: 토큰이 유효하지 않음 (null or empty). URL={}", url);
+
+			return;
+		}
 		try {
 			Message.Builder messageBuilder = Message.builder()
 				.setToken(token)
@@ -24,9 +29,12 @@ public class FcmService {
 				.putData("image", "https://pinback-image.s3.ap-northeast-2.amazonaws.com/FCM-IMG.png")
 				.putData("url", url);
 
-			FirebaseMessaging.getInstance().send(messageBuilder.build());
+			String messageId = FirebaseMessaging.getInstance().send(messageBuilder.build());
+			log.info("FCM 메시지 전송 성공: Token={}, Message ID={}", token, messageId);
 
 		} catch (FirebaseMessagingException e) {
+			log.error("FCM API 호출 중 예외 발생: Token={}, Error Code={}, Error Message={}",
+				token, e.getErrorCode(), e.getMessage(), e);
 			throw new RuntimeException("익스텐션 알림 전송 실패", e);
 		}
 	}
