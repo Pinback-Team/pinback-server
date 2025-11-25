64b1a227bc168f7c4b516c833125f61886d0995a
ose0221 <ose0221@naver.com>
2025-11-25 18:51:45 +0900

    HOTFIX: 브라우저 자동 알림 비활성화

--- infrastructure/src/main/java/com/pinback/infrastructure/firebase/FcmService.java
+++ infrastructure/src/main/java/com/pinback/infrastructure/firebase/FcmService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.Notification;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,11 +20,8 @@ public class FcmService {
 		try {
 			Message.Builder messageBuilder = Message.builder()
 				.setToken(token)
-				.setNotification(Notification.builder()
-					.setTitle("pinback")
-					.setBody("저장한 북마크를 확인해보세요")
-					.setImage("https://pinback-image.s3.ap-northeast-2.amazonaws.com/FCM-IMG.png")
-					.build())
+				.putData("title", "pinback")
+				.putData("body", "저장한 북마크를 확인해보세요")
 				.putData("image", "https://pinback-image.s3.ap-northeast-2.amazonaws.com/FCM-IMG.png")
 				.putData("url", url);
 
