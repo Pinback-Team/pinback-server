f60b73af9d6b55753673b2a15a9687549354eb84
ose0221 <ose0221@naver.com>
2026-02-20 00:07:41 +0900

    feat: 공유 아티클 상세 조회 response에 url 추가

--- application/src/main/java/com/pinback/application/article/dto/response/SharedArticleDetailResponse.java
+++ application/src/main/java/com/pinback/application/article/dto/response/SharedArticleDetailResponse.java
@@ -5,13 +5,15 @@ import com.pinback.domain.article.entity.Article;
 public record SharedArticleDetailResponse(
 	long articleId,
 	String ownerName,
-	String memo
+	String memo,
+	String url
 ) {
 	public static SharedArticleDetailResponse from(Article article) {
 		return new SharedArticleDetailResponse(
 			article.getId(),
 			article.getUser().getUsername(),
-			article.getMemo()
+			article.getMemo(),
+			article.getUrl()
 		);
 	}
 }
